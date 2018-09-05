954cfd3625107a27f5dee98e0ce09c66c5d42858
Navid <31672446+sky2464@users.noreply.github.com>
2018-09-04 22:42:55 -0400

    FlaskApp update1

--- Project1/FlaskApp.py
+++ Project1/FlaskApp.py
@@ -1,11 +1,38 @@
 import sqlite3
-
+from flask import Flask, redirect, url_for, request, flash, g, render_template, session
+from functools import wraps
 
 connection = sqlite3.connect('database.db')
 
 cursor = connection.cursor()
 
-# Insert the Navid user for test
+
+connection.close()
+# Create Flask object
+app = Flask(__name__)
+
+# index
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+# Main page
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+def items():
+	g.db= connection()
+	if request.methods == 'POST':
+		newItem = request.form ['New_Item']
+		cursor = g.db.execute("SELECT rowid From items WHERE name = ? ", (newItem,))
+		theFetch =cursor.fetchone()
+
+
+
+'''
+	# Insert the Navid user for test
 sql = "Insert into users (username) values ('Navid')"
 cursor.execute(sql)
 
@@ -22,10 +49,4 @@ rows = cursor.fetchall()
 
 for row in rows:
 	print(row)
-
-connection.close()
-
-
-
-
-
+'''
