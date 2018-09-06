6174ba7224f6098de912b73619b45a37cfe0e99c
Navid <31672446+sky2464@users.noreply.github.com>
2018-09-05 22:31:35 -0400

    update

--- Project1/FlaskApp.py
+++ Project1/FlaskApp.py
@@ -1,65 +1,97 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, g
 import sqlite3
-from flask import Flask, redirect, url_for, request, flash, g, render_template, session
 from functools import wraps
-
-connection = sqlite3.connect('database.db')
-
-cur = connection.cursor()
-cur.execute("Create TABLE Users (users_name text) ")
-cur.execute("Create TABLE items (items_names text)")
-cur.execute("DROP TABLE items")
-cur.execute("CREATE TABLE items(id INT, name Text)")
-
-
 
 # Create Flask object
 app = Flask(__name__)
 
-# index
+app.database="database.db"
 
+def connection_db():
+	return sqlite3.connect(app.database)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
+		flash('Hereeeeeeeeee i goooooooooooooo')
 		session['logged_in'] = True
 		flash('You were just logged in.')
-		return redirect(url_for('item'))
+		return redirect("todo.html")
 	return render_template('index.html')
+	
+@app.route('/login')
+def login():
+	flash('Welcome')
+	return render_template("todo.html")
+		
+@app.route('/logout')
+def logout():
+	flash('Logged out')
+	return render_template("index.html")	
+	
+@app.route("/newuser")
+def newuser():
+	#return render_template("newuser.html")
+	return render_template('newuser.html')
 
-# Main page
+# to do list
+@app.route('/todo')
+#def todo():
+		#return render_template('todo.html')
+def item():
+	g.db = connection_db()
+	if request.method == 'POST':
+		if 'new' in request.form:
+			addList = request.form['new']
+			cur = g.db.execute("SELECT rowid FROM posts WHERE List = ?", (addList,))
+			data = cur.fetchone()
+			if data is None:
+				# Insert new item
+				cur = g.db.execute('INSERT into posts (List,Note) VALUES (?,?)', (addList,'Not Done'))
+				g.db.commit()
+			cur = g.db.execute('select * from posts')
+			posts = [dict(List=row[0],Note=row[1]) for row in cur.fetchall()]
+			g.db.close()
+			return render_template('todo.html',posts=posts)
+		elif 'delete' in request.form:
+			deleteList = request.form['delete']
+			# Delete item 
+			cur = g.db.execute('DELETE FROM posts WHERE List = ?', (deleteList,))
+			g.db.commit()
+			cur = g.db.execute('select * from posts')
+			posts = [dict(List=row[0],Note=row[1]) for row in cur.fetchall()]
+			g.db.close()
+			return render_template('todo.html',posts=posts)
+		elif 'mark' in request.form:
+				markItem = request.form['mark']
+				''' Mark item '''
+				cur = g.db.execute('UPDATE posts SET Status = ? WHERE Item = ?', ('Done', markItem))
+				g.db.commit()
+				cur = g.db.execute('select * from posts')
+				posts = [dict(Item=row[0],Status=row[1]) for row in cur.fetchall()]
+				g.db.close()
+				return render_template('item.html',posts=posts)
+	else:
+		cur = g.db.execute('select * from posts')
+		posts = [dict(List=row[0],Note=row[1]) for row in cur.fetchall()]
+		g.db.close()
+		return render_template('todo.html',posts=posts)
+			
 
 
-@app.route('/todo', methods=['GET', 'POST'])
-def items():
-	g.db= connection()
-	if request.methods == 'POST':
-		newItem = request.form ['New_Item']
-		#cursor = 
-		connection.execute("SELECT rowid From items WHERE name = ?", (newItem,))
-		#theFetch =cursor.fetchone()
-	return render_template('todo.html')
+'''create account '''
+@app.route('/create', methods=['GET','POST'])
+def create():		
+	return render_template("newuser.html")
+	
+@app.route('/register')
+def register():
+	#return redirect(url_for('index'))
+	flash('Now log in')
+	return render_template("home.html")
 
-connection.close()
 
-'''
-	# Insert the Navid user for test
-sql = "Insert into users (username) values ('Navid')"
-cursor.execute(sql)
 
-# Insert the project1 items for test
-sql = "Insert into items (name) values ('Project1')"
-cursor.execute(sql)
-
-connection.commit()
-
-sql = 'select *from  users'
-cursor.execute(sql)
-
-rows = cursor.fetchall()
-
-for row in rows:
-	print(row)
-'''
-
-if __name__ == '__main__':
+	
+if __name__=='__main__':
 	app.run(debug=True)
